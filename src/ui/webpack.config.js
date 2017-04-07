c7850ab919e46db2483a936d9c40ef601f059b84
unknown <bhansalr@BHANSALR-03-L.advisory.com>
2017-04-07 09:21:22 -0500

    Home page (in progress)

---
+++ src/ui/webpack.config.js
@@ -0,0 +1,8 @@
+/* 
+Module export tells that this file exports an object which is going to represent our configurations for Webpack
+*/
+module.exports = {
+  entry: [
+    './app/index.js'
+  ]
+}
